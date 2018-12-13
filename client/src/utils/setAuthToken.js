92f4df2253cca65a0c7625926151452f1d88b828
Arjay Isidro <arjay@Arjays-MacBook-Pro.local>
2018-12-13 10:10:52 +0800

    login and logout checkpoint

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -6,7 +6,7 @@ const setAuthToken = token => {
     axios.defaults.headers.common['Authorization'] = token;
   } else {
     // Delete header
-    delete axios.default.headers.common['Authorization'];
+    delete axios.defaults.headers.common['Authorization'];
   }
 };
 
