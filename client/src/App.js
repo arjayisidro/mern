faa51e8ab55ab006f9488824c1b252919030df38
Arjay Isidro <arjay@Arjays-MacBook-Pro.local>
2018-12-17 15:33:57 +0800

    Checkpoint: dashboard auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, LogoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileAction';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -13,6 +16,9 @@ import Footer from './components/layout/Footer';
 
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 
@@ -31,6 +37,7 @@ if (localStorage.jwtToken) {
     // Logout the user
     store.dispatch(LogoutUser());
     // TODO: Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
@@ -47,6 +54,14 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { LogoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileAction';
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.LogoutUser();
   }
 
@@ -93,5 +95,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { LogoutUser }
+  { LogoutUser, clearCurrentProfile }
 )(Navbar);
